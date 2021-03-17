bf16c8550e8324ee205a1cf21beabc011535a2f7
M.Konstantinovna <76544214+locaIhost@users.noreply.github.com>
2021-03-17 02:36:43 +0200

    Update connect.pl
    Add sub connect and send beep.

--- connect.pl
+++ connect.pl
@@ -29,5 +29,35 @@
 #####################################
 use Net::OpenSSH;
 use strict;
-use warnings;
 #####################################
+#Scalar data for connect	=<1>=~
+#####################################
+my ( $name, $addr, $pass, $off, $beep );
+#####################################
+#Config yOu net data		  =<2>=~
+#####################################
+( $name ) = qq(YOU_NAME);
+( $addr ) = qq(10.101.10.101);
+( $pass ) = "YOUR_PASSWORD";
+( $off ) = int(7);
+#####################################
+#Sub for connect and send beep =<3>~
+#####################################
+sub connect_ssh 
+{
+	my ( $get ) = Net::OpenSSH->new
+	(
+    $addr,
+    user        => $name,
+    password    => $pass,
+    timeout     => $off,
+    master_opts => [ -o => "StrictHostKeyChecking=no" ]
+	);
+	( $beep ) = ("beep frequency=35 length=0.1");
+	( $get )->capture( $beep ) or die "I want c0ff33";
+};
+
+&connect_ssh
+
+
+__END__
